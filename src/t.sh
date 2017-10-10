88b496257964a9d6c591023de4535d4c012b0c98
Gabriel Lievano <gabriell@google.com>
2017-10-10 11:57:17 -0700

    Adds edit and help options.

--- src/t.sh
+++ src/t.sh
@@ -1,14 +1,52 @@
 #!/usr/bin/env bash
 
+# TODO: make this configurable
+EDITOR="emacsclient -t"
+
+function usage {
+  echo "To edit task list directly: t -e"
+  echo "To add a task to list: t <task description>"
+  echo "To display task list: t"
+  exit
+}
+
 # Check if ~/.t exists
 if [ ! -d ~/.t ]; then
   mkdir ~/.t
   touch ~/.t/list.org
 fi
 
-# Check if arguments passed. If so, input them to list. If not, display list.
-if [ $# -eq 0 ]; then
-  cat ~/.t/list.org
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+  case $key in
+    -h|--help)
+      HELP=true
+      shift
+      ;;
+    -e|--edit)
+      EDIT=true
+      shift
+      ;;
+    *)
+      POSITIONAL+=("$1")
+      shift
+      ;;
+  esac
+done
+set -- "${POSITIONAL[@]}"
+
+if [ -n "$HELP" ]; then
+  usage
+fi
+
+if [ -n "$EDIT" ]; then
+  $EDITOR ~/.t/list.org
 else
-  echo "* TODO:" $@ >> ~/.t/list.org
+  if [ ${#POSITIONAL[@]} -eq 0 ]; then
+    cat ~/.t/list.org
+  else
+    echo "* TODO: ${POSITIONAL[@]}" >> ~/.t/list.org
+  fi
 fi
